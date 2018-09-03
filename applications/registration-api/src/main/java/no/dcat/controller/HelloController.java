038fb51692fe29dbdd8d39369e28a5d4bb517d88
David Skogan <david.skogan@gmail.com>
2018-09-03 13:30:26 +0200

    Renamed methods and experimented with various spring boot settings to enable saml logout. The application mostly failed with springSecurityFilterChain AlreadyBuiltException. Hopefully this will not occur with @Lazy annotation on CustomRepositoryConfigurer

--- applications/registration-api/src/main/java/no/dcat/controller/HelloController.java
+++ applications/registration-api/src/main/java/no/dcat/controller/HelloController.java
@@ -10,23 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/home")
-    String home(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        SAMLUserDetails userDetails = (SAMLUserDetails) authentication.getPrincipal();
-        model.addAttribute("itworks", "YES");
-        model.addAttribute("fnr", userDetails.getAttribute("uid"));
-        model.addAttribute("SecurityLevel", userDetails.getAttribute("SecurityLevel"));
-        model.addAttribute("Culture", userDetails.getAttribute("Culture"));
-        model.addAttribute("OnBehalfOf", userDetails.getAttribute("OnBehalfOf"));
-        model.addAttribute("epostadresse", userDetails.getAttribute("epostadresse"));
-        model.addAttribute("mobiltelefonnummer", userDetails.getAttribute("mobiltelefonnummer"));
-        model.addAttribute("reservasjon", userDetails.getAttribute("reservasjon"));
-        model.addAttribute("status", userDetails.getAttribute("status"));
-
-        return "home";
-    }
-
     @RequestMapping("/loggetut")
     String loggetut(Model model) {
 
